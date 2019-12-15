bfc746db2af4b31f0cc9bd1994cf358a8b082868
航游四海 <wuyuhang@live.cn>
2019-12-15 23:31:34 +0800

    Update CircularLinkedList.h

---
+++ CircularLinkedList.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define OK 1
+#define ERROR 0
+
+typedef int ElemType;
+
+// 定义循环链表结点类型
+typedef struct LNode {
+    ElemType data;          // 数据域
+    struct LNode* next;     // 指针域
+}LNode, *LinkList;          // LinkList为指向结构体LNode的指针类型
+
+// 遍历操作
+int PrintList(LinkList L)
+{
+    if (L == NULL) {
+        return ERROR;
+    }
+
+    LNode* p = L->next;
+
+    while (p != L) {
+        printf("%d", p->data);
+
+        p = p->next;
+    }
+    
+    return OK;
+}
