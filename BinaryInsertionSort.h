970a54ccb5fcd1dde380b03e4e3d6007043294a2
航游四海 <wuyuhang@live.cn>
2019-12-18 00:02:21 +0800

    Create BinaryInsertionSort.h

---
+++ BinaryInsertionSort.h
@@ -0,0 +1,56 @@
+// 待排序记录的数据类型定义
+#define MAXSIZE 50              // 顺序表的最大长度
+
+typedef int KeyType;            // 定义关键字类型为整型
+
+typedef struct {
+    KeyType key;                // 关键字项
+    InfoType otherinfo;         // 其它数据项
+}RedType;                       // 记录类型
+
+typedef struct {
+    RedType r[MAXSIZE + 1];     // r[0]闲置或用做哨兵
+    int length;                 // 顺序表长度
+}SeqList;                       // 顺序表类型
+
+// 折半插入排序
+// 时间复杂度：O(n²)
+// 空间复杂度：O(1)
+// 稳定性：稳定
+void BInserSort(SeqList &L)
+{
+    int i = 0;
+    int j = 0;
+    int low = 0;
+    int high = 0;
+    int mid = 0;
+
+    // 依次将L.r[2]~L.r[L.length]插入到前面已排序序列
+    for (i = 2; i <= L.length; ++i) {
+        // 将L.r[i]暂存到L.r[0]
+        L.r[0] = L.r[i]; 
+
+        // 设置折半查找的范围       
+        low = 1;
+        high = i - 1;
+
+        // 折半查询（默认递增有序）
+        while (low <= high) {
+            // 取中间点
+            mid = (low + high) / 2;
+
+            if (L.r[0].key < L.r[mid].key) {
+                high = mid - 1;                 // 查找左半子表
+            } else {
+                low = mid + 1;                  // 查找右半子表
+            }
+        }
+        
+        for (j = i - 1; j >= high + 1; --j) {
+            L.r[j + 1] = L.r[j];                // 统一后移元素，空出插入位置
+        }
+
+        // 将r[0]即原r[i]，插入到正确位置
+        L.r[high + 1] = L.r[0];
+    }
+}
