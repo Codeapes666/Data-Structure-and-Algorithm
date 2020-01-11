1659f1d2080f30fdacbc74d9c6bfc0234ec9f5a5
航游四海 <wuyuhang@live.cn>
2020-01-11 23:06:39 +0800

    Create HashSearch.h

---
+++ HashSearch.h
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define OK          1
+#define ERROR       0
+#define OVERFLOW   -1
+
+typedef int Status;
+
+#define HASHSIZE 12             // 定义散列表长为数组的长度
+#define NULLKEY -32768
+
+typedef struct {
+    int* elem;                  // 数据元素存储基址，动态分配数组
+    int count;                  // 当前数据元素个数
+}HashTable;
+
+int m = 0;                      // 散列表表长，全局变量
+
+// 初始化散列表
+Status initHashTable(HashTable* HT)
+{
+    int i = 0;
+    m = HASHSIZE;
+    HT->count = m;
+    HT->elem = (int*)malloc(sizeof(int) * m);
+    if (HT->elem == NULL) {
+        exit(OVERFLOW);
+    }
+
+    for (i = 0; i < m; ++i) {
+        HT->elem[i] = NULLKEY;
+    }
+
+    return OK;
+}
+
+// 构造散列函数
+Status Hash(int key)
+{
+    return key % m;                         // 构造方法为除留余数法
+}
+
+// 插关键字进入散列表
+Status InsertHash(HashTable* HT, int key)
+{
+    int addr = Hash(key);                   // 求散列函数
+
+    while (HT->elem[addr] != NULLKEY) {     // 如果不为空，则冲突
+        addr = (addr + 1) % m;              // 开放定址法的线性探测
+        HT->elem[addr] = key;               // 直到有空位后插入关键字
+    }
+
+    return OK;
+}
+
+// 散列表的查找
+Status SearchHash(HashTable HT, int key, int* addr)
+{
+    *addr = Hash(key);                      // 求散列地址
+
+    while (HT.elem[*addr] != key) {
+        *addr = (*addr + 1) % m;            // 开放定址法的线性探测
+
+        // 如果循环回到原点，则说明关键字不存在，返回ERROR
+        if (HT.elem[*addr] == NULLKEY || *addr == Hash(key)) {
+            return ERROR;
+        }
+    }
+}
