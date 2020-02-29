39b33f0ebbf4f956d7db14cbfddcb608b9641303
航游四海 <wuyuhang@live.cn>
2020-02-29 23:06:20 +0800

    Update QuickSort.h

--- QuickSort.h
+++ QuickSort.h
@@ -13,32 +13,32 @@ typedef struct {
 
 
 // Partition为分割函数
-int Partition(SeqList &L, int low, int high) {
+int Partition(SeqList* L, int low, int high) {
     // 用子表的第一个记录做基准值
-    L.r[0] = L.r[low];
+    L->r[0] = L->r[low];
 
     // 基准值关键字保存在pivotkey中
-    int pivotkey = L.r[low].key; 
+    int pivotkey = L->r[low].key; 
 
     // 循环跳出条件
     while (low < high) {
-        while (low < high && L.r[high].key >= pivotkey) {
+        while (low < high && L->r[high].key >= pivotkey) {
             --high;
         }
 
         // 将比基准值小的元素移动到左端
-        L.r[low] = L.r[high];
+        L->r[low] = L->r[high];
 
-        while (low < high && L.r[low].key <= pivotkey) {
+        while (low < high && L->r[low].key <= pivotkey) {
             ++low;
         }
 
         // 将比基准值大的元素移动到右端
-        L.r[high] = L.r[low];
+        L->r[high] = L->r[low];
     }
 
     // 基准元素存放到最终位置
-    L.r[low] = L.r[0];
+    L->r[low] = L->r[0];
 
     // 返回基准值位置
     return low;
@@ -48,7 +48,7 @@ int Partition(SeqList &L, int low, int high) {
 // 平均时间复杂度：O(nlog2n)
 // 空间复杂度：O(log2n)
 // 稳定性：不稳定
-void QuickSort(SeqList &L, int low, int high) {
+void QuickSort(SeqList* L, int low, int high) {
 
     // 调用前置初值：low = 1; high = L.length;
     if (low < high) {
