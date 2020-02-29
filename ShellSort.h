4661f41fb76656102119cd24005f7484cf74e889
航游四海 <wuyuhang@live.cn>
2020-02-29 23:06:29 +0800

    Update ShellSort.h

--- ShellSort.h
+++ ShellSort.h
@@ -16,24 +16,24 @@ typedef struct {
 // 当n在某个特定范围内，时间复杂度：O(n^1.3)
 // 空间复杂度：O(1)
 // 稳定性：不稳定
-void ShellSort(SeqList &L, int dk)
+void ShellSort(SeqList* L, int dk)
 {
     // 增量变化
-    for (dk = L.length / 2; dk >= 1; dk = dk / 2) {
-        for (int i = dk + 1; i <= L.length; ++i) {
+    for (dk = L->length / 2; dk >= 1; dk /= 2) {
+        for (int i = dk + 1; i <= L->length; ++i) {
             // 需将L.r[i]插入有序增量子表
-            if (L.r[i].key < L.r[i - dk].key) {
+            if (L->r[i].key < L->r[i - dk].key) {
                 // 暂存在L.r[0]
-                L.r[0] = L.r[i];
+                L->r[0] = L->r[i];
 
                 int j = 0;
-                for (j = i -dk; j > 0 && L.r[0].key < L.r[j].key; j -= dk) {
+                for (j = i -dk; j > 0 && L->r[0].key < L->r[j].key; j -= dk) {
                     // 记录后移，直到找到插入位置
-                    L.r[j + dk] = L.r[j];
+                    L->r[j + dk] = L->r[j];
                 }
 
                 // 将r[0]即原r[i]，插入到正确位置
-                L.r[j + dk] = L.r[0];
+                L->r[j + dk] = L->r[0];
             }
         }
     }
