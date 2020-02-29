4696d2c0ff6a6e7866b823a9bfb7e7ba6272321a
航游四海 <wuyuhang@live.cn>
2020-02-29 23:06:18 +0800

    Update MergeSort.h

--- MergeSort.h
+++ MergeSort.h
@@ -15,8 +15,8 @@ typedef struct {
 }SeqList;                       // 顺序表类型
 
 // 归并相邻两个有序子序列
-// 将有序表R.r[low...mid]和R.r[mid + 1...high]归并为有序表L.r[low...high]
-void Merge(SeqList R, SeqList &L, int low, int mid, int high)
+// 将有序表R.r[low...mid]和R.r[mid + 1...high]归并为有序表L->r[low...high]
+void Merge(SeqList R, SeqList* L, int low, int mid, int high)
 {
     int i = low;
     int j = mid + 1;
@@ -25,20 +25,20 @@ void Merge(SeqList R, SeqList &L, int low, int mid, int high)
     // 将R中记录由小大并入L中
     while (i <= mid && j <= high) {
         if (R.r[i].key <= R.r[j].key) {
-            L.r[k] = R.r[i++];
+            L->r[k] = R.r[i++];
         } else {
-            L.r[k] = R.r[j++];
+            L->r[k] = R.r[j++];
         }
     }
 
     // 将剩余的R.r[low...mid]复制到L中
     while (i <= mid) {
-        L.r[k++] = R.r[i++];
+        L->r[k++] = R.r[i++];
     }
 
     // 将剩余的R.r[j...high]复制到L中
     while (j <= high) {
-        L.r[k++] = R.r[j++];
+        L->r[k++] = R.r[j++];
     }   
 }
 
@@ -46,10 +46,10 @@ void Merge(SeqList R, SeqList &L, int low, int mid, int high)
 // 时间复杂度：O(nlog2n)
 // 空间复杂度：O(n)
 // 稳定性：稳定
-void MergeSort(SeqList R, SeqList &L, int low, int high)
+void MergeSort(SeqList R, SeqList* L, int low, int high)
 {
     if (low == high) {
-        L.r[low] = R.r[low];
+        L->r[low] = R.r[low];
     } else {
         // 初始化结构体对象
         SeqList S = {NULL, 0};
@@ -65,12 +65,12 @@ void MergeSort(SeqList R, SeqList &L, int low, int high)
         int mid = (low + high) / 2;
         
         // 对子序列R.r[low...mid]递归归并排序，结果放入S.r[low...mid]
-        MergeSort(R, S, low, mid);
+        MergeSort(R, &S, low, mid);
 
         // 对子序列R.r[mid + 1...high]递归归并排序，结果放入S.r[mid + 1...high]
-        MergeSort(R, S, mid + 1, high);
+        MergeSort(R, &S, mid + 1, high);
 
-        // 将S.r[low...mid]和S.r[mid + 1...high]归并到L.r[low...high]
+        // 将S.r[low...mid]和S.r[mid + 1...high]归并到L->r[low...high]
         Merge(S, L, low, mid, high);
 
         if (S.r != NULL) {
