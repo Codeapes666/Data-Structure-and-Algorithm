2e6cd18128ed76518f0e75b4dcd837a249b556c9
航游四海 <wuyuhang@live.cn>
2020-03-21 20:44:33 +0800

    Update CircularLinkedList.h

--- CircularLinkedList.h
+++ CircularLinkedList.h
@@ -6,17 +6,25 @@
 typedef int ElemType;
 typedef int Status;
 
+Status Visit(ElemType e)
+{
+    printf("%d ", e);
+
+    return OK;
+}
+
 // 定义循环链表结点类型
-typedef struct LNode {
+typedef struct LNode 
+{
     ElemType data;          // 数据域
     struct LNode* next;     // 指针域
-} LNode, *LinkList;          // LinkList为指向结构体LNode的指针类型
+} LNode, *LinkList;         // LinkList为指向结构体LNode的指针类型
 
 // 循环单链表的操作和单链表基本一致，差别仅在于：
 // 当链表遍历时，判别当前指针p是否指向表尾结点的终止条件不同
 
 // 遍历操作
-Status PrintList(LinkList L)
+Status ListTraverse (LinkList L, Status (*Visit)(ElemType))
 {
     if (L == NULL) {
         return ERROR;
@@ -25,8 +33,7 @@ Status PrintList(LinkList L)
     LNode* p = L->next;
 
     while (p != L) {
-        printf("%d", p->data);
-
+        Visit (p->data);
         p = p->next;
     }
     
