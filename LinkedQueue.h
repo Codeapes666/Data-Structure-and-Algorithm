4799ad307e227550b43d84f0aaa0e745db893bff
航游四海 <wuyuhang@live.cn>
2020-03-21 21:37:26 +0800

    Update LinkedQueue.h

--- LinkedQueue.h
+++ LinkedQueue.h
@@ -9,19 +9,28 @@
 typedef int ElemType;
 typedef int Status;
 
+Status Visit (ElemType e)
+{
+    printf("%d ", e);
+
+    return OK;
+}
+
 // 队列的链式存储结构
-typedef struct QNode {              // 链式队列结点
+typedef struct QNode                // 链式队列结点
+{                                   
     ElemType data;                  // 数据域
     struct QNode* next;             // 指针域
 } QNode, *QueuePtr;
 
-typedef struct {                    // 链式队列
+typedef struct                      // 链式队列
+{                    
     QueuePtr front;                 // 队头指针，等价于QNode* front
     QueuePtr rear;                  // 队尾指针，等价于QNode* rear
 } LinkQueue;
 
 // 链队的初始化
-Status InitQueue(LinkQueue* Q)      // 构造一个只有头结点的空队
+Status InitQueue (LinkQueue* Q)      // 构造一个只有头结点的空队
 {   // 生成新结点作为头结点，队头和队尾指针指向头结点
     Q->front = Q->rear = (QNode*)malloc(sizeof(QNode));
 
@@ -36,7 +45,7 @@ Status InitQueue(LinkQueue* Q)      // 构造一个只有头结点的空队
 }
 
 // 判空
-bool QueueEmpty(LinkQueue Q)
+bool QueueEmpty (LinkQueue Q)
 {
     if (Q.front == NULL) {          // 此时队列被销毁
         exit(1);
@@ -50,7 +59,7 @@ bool QueueEmpty(LinkQueue Q)
 }
 
 // 销毁链队
-Status DestroyQueue(LinkQueue* Q)
+Status DestroyQueue (LinkQueue* Q)
 {
     if (Q->front == NULL) {
         return ERROR;
@@ -72,7 +81,7 @@ Status DestroyQueue(LinkQueue* Q)
 }
 
 // 清空链队
-Status ClearQueue(LinkQueue* Q)
+Status ClearQueue (LinkQueue* Q)
 {
     if (Q->front == NULL) {
         return ERROR;
@@ -94,7 +103,7 @@ Status ClearQueue(LinkQueue* Q)
 }
 
 // 求链队长度
-Status QueueLength(LinkQueue Q)
+int QueueLength (LinkQueue Q)
 {
     int length = 0;
     QueuePtr p = Q.front;
@@ -109,7 +118,7 @@ Status QueueLength(LinkQueue Q)
 }
 
 // 取队头元素
-ElemType GetHead(LinkQueue Q)
+ElemType GetHead (LinkQueue Q)
 {
     if (Q.front != Q.rear) {            // 队列非空
         return Q.front->next->data;     // 返回队头元素的值，队头指针不变
@@ -117,7 +126,7 @@ ElemType GetHead(LinkQueue Q)
 }
 
 // 入队
-Status EnQueue(LinkQueue* Q, ElemType e)
+Status EnQueue (LinkQueue* Q, ElemType e)
 {   // 为入队元素分配结点空间，用指针p指向
     QNode* p = (QNode*)malloc(sizeof(QNode));
 
@@ -133,7 +142,7 @@ Status EnQueue(LinkQueue* Q, ElemType e)
 }
 
 // 出队
-Status DeQueue(LinkQueue* Q, ElemType* e)
+Status DeQueue (LinkQueue* Q, ElemType* e)
 {   
     // 删除Q的队头元素，用e返回其值
     if (Q->front == Q->rear) {          // 若队列空，则返回ERROR
@@ -155,7 +164,7 @@ Status DeQueue(LinkQueue* Q, ElemType* e)
 }
 
 // 遍历链列
-Status QueueTraverse(LinkQueue Q)
+Status QueueTraverse (LinkQueue Q, Status (*Visit)(ElemType))
 {
     if (Q.front == NULL) {
         return ERROR;
@@ -164,7 +173,7 @@ Status QueueTraverse(LinkQueue Q)
     QueuePtr p = Q.front->next;
 
     while (p != NULL) {
-        printf("%d ", p->data);
+        Visit (p->data);
         p = p->next;
     }
 
