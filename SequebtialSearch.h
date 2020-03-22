5284273ac86c6fa228d87adae851ecf7eea6c53b
航游四海 <wuyuhang@live.cn>
2020-03-22 21:32:34 +0800

    Update SequebtialSearch.h

--- SequebtialSearch.h
+++ SequebtialSearch.h
@@ -1,22 +1,25 @@
 typedef int KeyType;
 
 // 待查找的数据元素类型定义
-typedef struct {
-    KeyType key;        // 关键字域
-}ElemType;
+typedef struct 
+{
+    KeyType key;            // 关键字域
+} ElemType;
 
-typedef struct {
-    ElemType* elem;     // 存储空间基地址，建表时按实际长度分配，ST.elem[0]闲置不用
-    int length;         // 当前表长度
-}SSTable;
+typedef struct 
+{
+    ElemType* elem;         // 存储空间基地址，建表时按实际长度分配，ST.elem[0]闲置不用
+    int length;             // 当前表长度
+} SSTable;
 
 // 顺序查找
 // 适用于线性表的顺序存储结构、链式存储结构
 // 时间复杂度：O(n)
 int SeqSearch(SSTable ST, KeyType key)
-{   // 在顺序表ST中顺序查找其关键字等于key的数据元素。
+{   
+    // 在顺序表ST中顺序查找其关键字等于key的数据元素。
     // 若找到，则函数值为该元素在表中的位置，否则为0
-    ST.elem[0].key = key;                                   // 哨兵
+    ST.elem[0].key = key;                                       // 哨兵
 
     int i = 0;
     for (int i = ST.length; ST.elem[i].key != key; --i);        // 从后往前找
