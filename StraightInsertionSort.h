9ae830850d68861b4fb5aaae18fb3ad86b1e349e
航游四海 <wuyuhang@live.cn>
2020-03-22 21:32:40 +0800

    Update StraightInsertionSort.h

--- StraightInsertionSort.h
+++ StraightInsertionSort.h
@@ -1,22 +1,27 @@
 // 待排序记录的数据类型定义
-#define MAXSIZE 50              // 顺序表的最大长度
+#define MAXSIZE     100         // 顺序表的最大长度
+#define OK          1
+#define ERROR       0
 
 typedef int KeyType;            // 定义关键字类型为整型
+typedef int Status;
 
-typedef struct {
+typedef struct
+{
     KeyType key;                // 关键字项
-}RedType;                       // 记录类型
+} RedType;                      // 记录类型
 
-typedef struct {
+typedef struct
+{
     RedType r[MAXSIZE + 1];     // r[0]闲置或用做哨兵
     int length;                 // 顺序表长度
-}SeqList;                       // 顺序表类型
+} SeqList;                      // 顺序表类型
 
 // 直接插入排序
 // 平均时间复杂度：O(n²)
 // 空间复杂度：O(1)
 // 稳定性：稳定
-void InsertSort(SeqList* L)
+Status InsertSort(SeqList* L)
 {
     for (int i = 2; i <= L->length; ++i) {      // 依次将L.r[2]~L.r[L.length]插入到前面已排序序列
         if (L->r[i].key < L->r[i - 1].key) {    // 若L.r[i]的关键码小于其前驱，需将L.r[i]插入有序表
@@ -32,6 +37,8 @@ void InsertSort(SeqList* L)
         }
 
     }
+
+    return OK;
 }
 
 
