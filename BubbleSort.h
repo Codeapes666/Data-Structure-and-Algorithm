590bbaff142e26f49e1273b1708f98254f5ddd96
航游四海 <wuyuhang@live.cn>
2020-03-22 21:32:22 +0800

    Update BubbleSort.h

--- BubbleSort.h
+++ BubbleSort.h
@@ -1,24 +1,29 @@
-#define MAXSIZE 50              // 顺序表的最大长度
+#define MAXSIZE     100         // 顺序表的最大长度
+#define OK          1
+#define ERROR       0
 
 #define TRUE 1
 #define FALSE 0
 
 typedef int KeyType;            // 定义关键字类型为整型
+typedef int Status;
 
-typedef struct {
+typedef struct
+{
     KeyType key;                // 关键字项
-}RedType;                       // 记录类型
+} RedType;                      // 记录类型
 
-typedef struct {
+typedef struct
+{
     RedType r[MAXSIZE + 1];     // r[0]闲置或用做哨兵
     int length;                 // 顺序表长度
-}SeqList;                       // 顺序表类型
+} SeqList;                      // 顺序表类型
 
 // 冒泡排序
 // 平均时间复杂度：O(n²)
 // 空间复杂度：O(1)
 // 稳定性：稳定
-void BubbleSort(SeqList* L)
+Status BubbleSort(SeqList* L)
 {
     int m = L->length - 1;
     int flag = 1;               // flag用来标记某一趟排序是否发生交换
@@ -40,10 +45,12 @@ void BubbleSort(SeqList* L)
         }
         --m;
     }
+
+    return OK;
 }
 
 // 冒泡排序第2种方法
-void BubbleSort2(SeqList* L)
+Status BubbleSort2(SeqList* L)
 {
     int i = 0;
     int j = 0;
@@ -69,4 +76,6 @@ void BubbleSort2(SeqList* L)
             return;                // 本趟遍历后没有发生交换，说明表已经有序
         }
     }
+
+    return OK;
 }
