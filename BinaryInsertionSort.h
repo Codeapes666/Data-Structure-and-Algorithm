d0cccd8f43a06dc8c24c3ddbaf1d6410b46ac63e
航游四海 <wuyuhang@live.cn>
2020-03-22 21:32:16 +0800

    Update BinaryInsertionSort.h

--- BinaryInsertionSort.h
+++ BinaryInsertionSort.h
@@ -1,22 +1,27 @@
 // 待排序记录的数据类型定义
-#define MAXSIZE 50              // 顺序表的最大长度
+#define MAXSIZE     100         // 顺序表的最大长度
+#define OK          1
+#define ERROR       0
 
 typedef int KeyType;            // 定义关键字类型为整型
+typedef int Status;
 
-typedef struct {
+typedef struct
+{
     KeyType key;                // 关键字项
-}RedType;                       // 记录类型
+} RedType;                      // 记录类型
 
-typedef struct {
+typedef struct
+{
     RedType r[MAXSIZE + 1];     // r[0]闲置或用做哨兵
     int length;                 // 顺序表长度
-}SeqList;                       // 顺序表类型
+} SeqList;                      // 顺序表类型
 
 // 折半插入排序
 // 平均时间复杂度：O(n²)
 // 空间复杂度：O(1)
 // 稳定性：稳定
-void BInserSort(SeqList* L)
+Status BInserSort(SeqList* L)
 {
     // 依次将L.r[2]~L.r[L.length]插入到前面已排序序列
     for (int i = 2; i <= L->length; ++i) {
@@ -33,17 +38,19 @@ void BInserSort(SeqList* L)
             int mid = (low + high) / 2;
 
             if (L->r[0].key < L->r[mid].key) {
-                high = mid - 1;                 // 查找左半子表
+                high = mid - 1;                     // 查找左半子表
             } else {
-                low = mid + 1;                  // 查找右半子表
+                low = mid + 1;                      // 查找右半子表
             }
         }
         
         for (int j = i - 1; j >= high + 1; --j) {
-            L->r[j + 1] = L->r[j];              // 统一后移元素，空出插入位置
+            L->r[j + 1] = L->r[j];                  // 统一后移元素，空出插入位置
         }
 
         // 将r[0]即原r[i]，插入到正确位置
         L->r[high + 1] = L->r[0];
     }
+
+    return OK;
 }
