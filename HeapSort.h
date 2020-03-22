557f4597070064ddeedf9842ff9993da1ece342e
航游四海 <wuyuhang@live.cn>
2020-03-22 21:32:27 +0800

    Update HeapSort.h

--- HeapSort.h
+++ HeapSort.h
@@ -1,19 +1,24 @@
 // 待排序记录的数据类型定义
-#define MAXSIZE 50              // 顺序表的最大长度
+#define MAXSIZE     100         // 顺序表的最大长度
+#define OK          1
+#define ERROR       0
 
 typedef int KeyType;            // 定义关键字类型为整型
+typedef int Status;
 
-typedef struct {
+typedef struct
+{
     KeyType key;                // 关键字项
-}RedType;                       // 记录类型
+} RedType;                      // 记录类型
 
-typedef struct {
+typedef struct
+{
     RedType r[MAXSIZE + 1];     // r[0]闲置或用做哨兵
     int length;                 // 顺序表长度
-}SeqList;                       // 顺序表类型
+} SeqList;                      // 顺序表类型
 
 // 向下调整堆
-void HeapAdjustDown(SeqList* L, int s, int m)
+Status HeapAdjustDown(SeqList* L, int s, int m)
 {// 假设r[s+1...m]已经是堆，将r[s...m]调整为以r[s]为根的大根堆
     L->r[0] = L->r[s];
 
@@ -35,10 +40,12 @@ void HeapAdjustDown(SeqList* L, int s, int m)
     }
     // 被筛选结点的值放入最终位置
     L->r[s] = L->r[0];
+
+    return OK;
 }
 
 // 向上调整堆
-void HeapAdjustUp(SeqList* L, int m) 
+Status HeapAdjustUp(SeqList* L, int m) 
 {// 参数m为向上调整的结点，也为堆的元素个数
     L->r[0] = L->r[m];
 
@@ -55,23 +62,27 @@ void HeapAdjustUp(SeqList* L, int m)
     }
     // 复制到最终位置
     L->r[m] = L->r[0];
+
+    return OK;
 }
 
 // 初始建堆
-void CreatHeap(SeqList* L)
+Status CreatHeap(SeqList* L)
 {
     int n = L->length;
 
     for (int i = n / 2; i > 0; --i) {
         HeapAdjustDown(L, i, n);
     }
+
+    return OK;
 }
 
 // 堆排序
 // 时间复杂度：O(nlog2n)
 // 空间复杂度：O(1)
 // 稳定性：不稳定
-void HeapSort(SeqList* L)
+Status HeapSort(SeqList* L)
 {
     CreatHeap(L);
 
@@ -84,4 +95,6 @@ void HeapSort(SeqList* L)
         // 将L->r[1...i-1]重新调整为大根堆
         HeapAdjustDown(L, 1, i - 1);
     }
+
+    return OK;
 }
