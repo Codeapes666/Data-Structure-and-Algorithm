74b1eaba32a4c9a71fa0a807a56d560f7129a52a
航游四海 <wuyuhang@live.cn>
2020-03-22 21:32:38 +0800

    Update SimpleSelectionSort.h

--- SimpleSelectionSort.h
+++ SimpleSelectionSort.h
@@ -1,22 +1,27 @@
 // 待排序记录的数据类型定义
-#define MAXSIZE 50              // 顺序表的最大长度
+#define MAXSIZE     100         // 顺序表的最大长度
+#define OK          1
+#define ERROR       0
 
 typedef int KeyType;            // 定义关键字类型为整型
+typedef int Status;
 
-typedef struct {
+typedef struct
+{
     KeyType key;                // 关键字项
-}RedType;                       // 记录类型
+} RedType;                      // 记录类型
 
-typedef struct {
+typedef struct
+{
     RedType r[MAXSIZE + 1];     // r[0]闲置或用做哨兵
     int length;                 // 顺序表长度
-}SeqList;                       // 顺序表类型
+} SeqList;                      // 顺序表类型
 
 // 简单选择排序
 // 时间复杂度：O(n²)
 // 空间复杂度：O(1)
 // 稳定性：不稳定
-void SelectSort(SeqList* L)
+Status SelectSort(SeqList* L)
 {
     for (int i = 1; i < L->length; ++i) {           // 一共进行L.length - 1趟
         int min = i;                                // 记录最小元素位置
@@ -33,4 +38,6 @@ void SelectSort(SeqList* L)
             L->r[min] = temp;
         }
     }
+    
+    return OK;
 }
